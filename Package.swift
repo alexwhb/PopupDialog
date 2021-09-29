0ed0ee8bd384e189929f24c332246a31d6145f03
Alex Black <alex.black@blackstoneaudio.com>
2021-09-29 14:40:03 -0700

    updates swift pm dependencies.

--- Package.swift
+++ Package.swift
@@ -4,28 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "PopupDialog",
-    platforms: [
-        .iOS(.v10)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "PopupDialog",
-            targets: ["PopupDialog"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(
-            name: "DynamicBlurView",
+        name: "PopupDialog",
+        platforms: [
+            .iOS(.v10)
+        ],
+        products: [
+            // Products define the executables and libraries a package produces, and make them visible to other packages.
+            .library(
+                    name: "PopupDialog",
+                    targets: ["PopupDialog"])
+        ],
+        dependencies: [
+            // Dependencies declare other packages that this package depends on.
+            .package(
+                    name: "DynamicBlurView",
             url: "https://github.com/KyoheiG3/DynamicBlurView",
             from: "5.0.0"),
-        .package(
-            name: "Nimble",
-            path: "Example/Nimble"),
-        .package(
-            name: "FBSnapshotTestCase",
-            path: "Example/iOSSnapshotTestCase")
+//         .package(
+//             name: "Nimble",
+//             path: "Example/Nimble"),
+//        .package(
+//            name: "FBSnapshotTestCase",
+//            path: "Example/iOSSnapshotTestCase")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
